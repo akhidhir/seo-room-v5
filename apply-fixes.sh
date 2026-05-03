2d2e6734dcd2fd96e02cbd3b4bba1987af4700ab
Ayad Khudhur <akhudhur65@gmail.com>
2026-05-03 23:29:43 +0800

    auto-create CWV findings + action items from speed audit results

---
+++ apply-fixes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Delete this file after use
+rm -f ~/Desktop/seo-room-v5/apply-fixes.sh
---
+++ sync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This file is just a marker to test Write tool sync
+echo "Write tool syncs to disk"
